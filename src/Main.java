68677bd572f1ddf7eb9d4d4affb6523d05002040
Katya <dolgova01011987@gmail.com>
2024-02-21 16:57:37 +0300

    Initiall commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 100;
-        int replenishment = 1200;
+        int replenishment = 1100;
         int bonus;
         if (replenishment > 1000) {
             bonus = replenishment / 100;
